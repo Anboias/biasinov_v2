4b178a6c8569a273489ea764bd0519270de6da5d
Anboias <biasinovschi@gmail.com>
2020-11-05 22:06:12 +0200

    finished buttons/anchors

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 window.onscroll = function() {myFunction()};
 
-let seeMoreButton = document.getElementById("h-button");   
-let seeMoreButtonAbout = document.getElementById("a-button");   
+let hButtonAbout = document.getElementById("h-button-about");   
+let aButtonContact = document.getElementById("a-button-contact");   
+let nAnchorHome = document.getElementById("n-a-home");   
+let nAnchorAbout = document.getElementById("n-a-about");   
+let nAnchorContact = document.getElementById("n-a-contact");   
+let stickyButtonHome = document.getElementById("sticky-button-home");
 
-let windowHeight = document.documentElement.clientWidth;
 
+/* Smooth scroll Navbar */
 function myFunction() {
-  if (document.body.scrollTop > windowHeight || document.documentElement.scrollTop > windowHeight) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     document.getElementById("navbar").classList.add("nav-compact")
+    stickyButtonHome.style.display = "block";
+
   } else {
     document.getElementById("navbar").classList.remove("nav-compact") = "";
+    stickyButtonHome.style.display = "none";
+
   }
 }
 
-const doScroll = () => {  
-  let about = document.getElementById("about");
-  about.scrollIntoView({behavior: 'smooth'});
+/* Smooth scroll buttons/anchors */
+const doScroll = (event) => {  
+  let goTo = event.target.id ? event.target.id : event.target.parentElement.id;
+  // let goTo = event.target.id;
+  console.log(goTo)
+
+  goTo = goTo.substring(goTo.lastIndexOf('-') + 1);
+
+
+  let goToElement = document.getElementById(goTo);
+  goToElement.scrollIntoView({behavior: 'smooth'});
 } 
 
-const doScrollContact = () => {  
-  let about = document.getElementById("contact");
-  about.scrollIntoView({behavior: 'smooth'});
-} 
+hButtonAbout.addEventListener("click", doScroll);
+aButtonContact.addEventListener("click", doScroll);
+nAnchorHome.addEventListener("click", doScroll);
+nAnchorAbout.addEventListener("click", doScroll);
+nAnchorContact.addEventListener("click", doScroll);
+stickyButtonHome.addEventListener("click", doScroll);
 
-seeMoreButton.addEventListener("click", doScroll);
-seeMoreButtonAbout.addEventListener("click", doScrollContact);
