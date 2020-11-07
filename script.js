ab10906abbd85c85c854ef93eb47a4b017c2be8c
Anboias <biasinovschi@gmail.com>
2020-11-07 20:35:27 +0200

    added hire me & resume buttons

--- script.js
+++ script.js
@@ -1,43 +1,61 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction();
+};
 
-let hButtonAbout = document.getElementById("h-button-about");   
-let aButtonContact = document.getElementById("a-button-contact");   
-let nAnchorHome = document.getElementById("n-a-home");   
-let nAnchorAbout = document.getElementById("n-a-about");   
-let nAnchorContact = document.getElementById("n-a-contact");   
-let stickyButtonHome = document.getElementById("sticky-button-home");
-
+let navbar = document.getElementById('navbar');
+let hButtonAbout = document.getElementById('h-button-about');
+let aButtonContact = document.getElementById('a-button-contact');
+let nAnchorHome = document.getElementById('n-a-home');
+let nAnchorAbout = document.getElementById('n-a-about');
+let nAnchorContact = document.getElementById('n-a-contact');
+let stickyButtonHome = document.getElementById('sticky-button-home');
+let profileImg = document.getElementById('profile-img');
 
 /* Smooth scroll Navbar */
 function myFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("navbar").classList.add("nav-compact")
-    stickyButtonHome.style.display = "block";
-
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    navbar.classList.add('nav-compact');
+    stickyButtonHome.style.display = 'block';
   } else {
-    document.getElementById("navbar").classList.remove("nav-compact") = "";
-    stickyButtonHome.style.display = "none";
+    navbar.classList.remove('nav-compact');
+    stickyButtonHome.style.display = 'none';
   }
 }
 
 /* Smooth scroll buttons/anchors */
-const doScroll = (event) => {  
+const doScroll = (event) => {
   let goTo = event.target.id ? event.target.id : event.target.parentElement.id;
 
   // let goTo = event.target.id;
-  console.log(goTo)
+  console.log(goTo);
 
   goTo = goTo.substring(goTo.lastIndexOf('-') + 1);
 
-
   let goToElement = document.getElementById(goTo);
-  goToElement.scrollIntoView({behavior: 'smooth'});
-} 
+  goToElement.scrollIntoView({ behavior: 'smooth' });
+};
 
-hButtonAbout.addEventListener("click", doScroll);
-aButtonContact.addEventListener("click", doScroll);
-nAnchorHome.addEventListener("click", doScroll);
-nAnchorAbout.addEventListener("click", doScroll);
-nAnchorContact.addEventListener("click", doScroll);
-stickyButtonHome.addEventListener("click", doScroll);
+const profileImgHovered = (event) => {
+  console.dir(event);
+  let animateClassName = 'animate__pulse';
 
+  if (event.type === 'mouseenter') {
+    profileImg.classList.add(animateClassName);
+  } else {
+    profileImg.classList.remove(animateClassName);
+  }
+
+  // console.dir(profileImg);
+};
+
+hButtonAbout.addEventListener('click', doScroll);
+aButtonContact.addEventListener('click', doScroll);
+nAnchorHome.addEventListener('click', doScroll);
+nAnchorAbout.addEventListener('click', doScroll);
+nAnchorContact.addEventListener('click', doScroll);
+stickyButtonHome.addEventListener('click', doScroll);
+profileImg.addEventListener('mouseenter', profileImgHovered);
+profileImg.addEventListener('mouseleave', profileImgHovered);
